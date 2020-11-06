0b4b8771eb88fa96aacf527b8b3c2642d327323a
Khaled Emad <khaledk212@gmail.com>
2020-11-06 22:30:37 +0200

    rendering html/css

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const express = require('express') 
+const path = require('path') 
 require('./db/mongoose') 
 const reportRouter = require('./routers/report') 
 
 const app = express() 
 const port = process.env.PORT || 3000 
+const htmlDir = path.join(__dirname, '../public') 
 
 app.use(express.json()) 
 app.use(reportRouter)
+app.use(express.static(htmlDir))
+
+
 
 app.listen(port, () => {
     console.log("Server is up at port " + port) 
--- src/routers/report.js
+++ src/routers/report.js
@@ -20,7 +20,7 @@ router.post('/reports', async (req, res) => {
 
 router.get('/reports', async (req, res) =>{
     
-    try {
+    try {cd
         const ret = await Report.find({})
         res.send(ret)
     } catch(e){
