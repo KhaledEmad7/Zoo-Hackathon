ef2227d95c2d981ab670c017e7d760b6212f5062
Khaled Emad <khaledk212@gmail.com>
2020-11-06 15:56:44 +0200

    Adding report router

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express()
 const port = process.env.PORT || 3000 
 
 app.use(express.json()) 
-app.use(reportRouter);
+// app.use(reportRouter)
 
 app.listen(port, () => {
     console.log("Server is up at port " + port) 
---
+++ src/routers/report.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const Report = require('../models/report')
+
+const router = express.Router()
+
+router.post('/reports', async (req, res) => {
+    const report = new Report(req.body) 
+
+    try{
+        await Report.save();
+
+        res.status(201).send(report) 
+
+    } catch(e){
+        res.status(400).send(e) 
+    }
+})
+
+
+router.get('/reports', async (req, res) =>{
+    
+    try {
+        const ret = await Report.find({})
+        res.send(ret)
+    } catch(e){
+        res.status(500).send()
+    }
+
+})
+
+
+router.delete('/reports/:id', async (req, res) => {
+    try{
+        const report = await Report.findByIdAndDelete(req.params.id)
+        
+        if(!report){
+            return res.status(404).send()
+        }
+        res.send(report)
+
+    } catch(e){
+        res.status(500).send()
+    }
+})
+
+module.exports = router
